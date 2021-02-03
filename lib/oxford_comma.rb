6f6be1a4d3cdb7aa6258f2f25c0f6989b366652f
MarkBezsonov <markbezsonov@gmail.com>
2021-02-02 21:29:38 -0600

    commit tree c76ef627cb7966e95a1d5ca3fb60a90efb513fd9

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,14 @@
-def oxford_comma(array)
+array = ["kiwi", "durian", "starfruit", "mangos", "dragonfruits"]
 
-end
+def oxford_comma(array)
+    if array.length == 1
+      return "#{array[0]}"
+    elsif array.length == 2
+      return array.join(" and ")
+    else array.length >= 3
+      new_array = "and #{array[-1]}"
+      array.pop
+      array.push(new_array)
+      return array.join(", ")
+    end
+  end
